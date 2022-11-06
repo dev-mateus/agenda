68a4e763e34c7dc1dd9c9510e6260afb395f5e02
Mateus Pereira <mateus.pereira4@gmail.com>
2022-11-06 09:49:13 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'agenda-02',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
